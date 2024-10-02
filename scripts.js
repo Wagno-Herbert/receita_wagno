e2bbae89e735084a6fc63155bae1a2238edebee9
wagno Herbert <betobahiano@gmail.com>
2024-10-02 20:22:39 -0300

    segundo commint

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/*
+[] Pegar a informação do input, quando o botão for clicado
+[] Ir até a API/Servidor. e trazer as receitas
+[] Colocar as receitas na tela
+[]  Pegar a informação do input, quando o botão for clicado
+[]  Ir até a API/Servidor. e trazer as receitas
+[]  Colocar as receitas na tela
+*/ 
